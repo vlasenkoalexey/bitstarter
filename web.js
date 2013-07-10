ba9d681138d1c256392c7417dde958397e8317e0
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2013-07-10 01:18:53 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var data = fs.readFileSync('index.html');
+  response.send(readFileSync.toString());
 });
 
 var port = process.env.PORT || 5000;
